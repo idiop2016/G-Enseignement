cc01211d2bfaa6ea4253642fc1cd0d7a300b36ff
Ibrahima DIOP <ibrahima.diop@univ-zig.sn>
2024-11-24 17:39:21 +0000

    initialisation du projet

---
+++ src/main/java/sn/uasz/genseignement/GEnseignementApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.genseignement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GEnseignementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GEnseignementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/uasz/genseignement/GEnseignementApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.genseignement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GEnseignementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
